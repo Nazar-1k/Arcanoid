d6d593aa13b2e635e9ba5e6f6b18cadbbd0753cc
Nazar Maksymenko <na.programmer.z@gmail.com>
2023-03-16 10:37:15 +0200

    Add and implement class Sprite

---
+++ Arcanoid/Arcanoid/Sprite.cpp
@@ -0,0 +1,142 @@
+#include "Sprite.h"
+
+Sprite::Sprite(const char* path, SDL_Renderer* renderer)
+	: renderer(renderer)
+{
+	loadFromFile(path);
+}
+
+Sprite::~Sprite()
+{
+	free();
+}
+
+void Sprite::initSprite(const char* path, SDL_Renderer* renderer)
+{
+	this->renderer = renderer;
+	loadFromFile(path);
+}
+
+bool Sprite::loadFromFile(const char* path)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path);
+	if (loadedSurface == NULL)
+	{
+		std::cout << "Unable to load image " << path << " SDL_image Error : \n" << IMG_GetError() << std::endl;
+		return texture != NULL;
+	}
+
+	//Color key image
+	SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+	//Create texture from surface pixels
+	newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+	if (newTexture == NULL)
+	{
+		std::cout << "Unable to create texture from " << path << "SDL Error: \n" << IMG_GetError() << std::endl;
+		return texture != NULL;
+	}
+
+	//Get image dimensions
+	width = loadedSurface->w;
+	height = loadedSurface->h;
+
+
+	//Get rid of old loaded surface
+	SDL_FreeSurface(loadedSurface);
+
+	//Return success
+	texture = newTexture;
+	SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+	is_Empty = texture != NULL;
+	return texture != NULL;
+}
+
+void Sprite::setColor(Uint8 red, Uint8 green, Uint8 blue)
+{
+	//Modulate texture rgb
+	SDL_SetTextureColorMod(texture, red, green, blue);
+}
+
+void Sprite::setBlendMode(SDL_BlendMode blending)
+{
+	//Set blending function
+	SDL_SetTextureBlendMode(texture, blending);
+}
+
+void Sprite::setAlpha(Uint8 alpha)
+{
+	//Modulate texture alpha
+	SDL_SetTextureAlphaMod(texture, alpha);
+}
+
+void Sprite::render(SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
+{
+
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { static_cast<int>(x),  static_cast<int>(y), width, height };
+
+	//Set clip rendering dimensions
+	if (clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+	//Render to screen
+	SDL_RenderCopyEx(renderer, texture, clip, &renderQuad, angle, center, flip);
+}
+
+void Sprite::getSize(int& w, int& h)
+{
+	w = this->width;
+	h = this->height;
+}
+
+void Sprite::setRenderer(SDL_Renderer* renderer)
+{
+	this->renderer = renderer;
+}
+
+void Sprite::setSize(int w, int h)
+{
+	this->width = w;
+	this->height = h;
+}
+
+void Sprite::setWith(int w)
+{
+	this->width = w;
+}
+
+void Sprite::setHeight(int h)
+{
+	this->height = h;
+}
+
+float Sprite::setX(float x)
+{
+	return this->x = x;
+}
+float Sprite::setY(float y)
+{
+	return this->y = y;
+}
+
+void Sprite::free()
+{
+	if (texture)
+	{
+		SDL_DestroyTexture(texture);
+		texture = nullptr;
+		renderer = nullptr;
+		width = 0;
+		height = 0;
+	}
+}
---
+++ Arcanoid/Arcanoid/Sprite.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+class Sprite
+{
+public:
+
+	Sprite() :width(0), height(0), texture(nullptr), renderer(nullptr),
+		angle(0.0), center(nullptr), flip(SDL_FLIP_NONE),
+		is_Empty(false), x(0), y(0) {}
+	Sprite(const char* path, SDL_Renderer* renderer);
+	~Sprite();
+
+	void initSprite(const char* path, SDL_Renderer* renderer);
+
+	//Loads image at specified path
+	bool loadFromFile(const char* path);
+
+	//Set color modulation
+	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+	//Set blending
+	void setBlendMode(SDL_BlendMode blending);
+
+	//Set alpha modulation
+	void setAlpha(Uint8 alpha);
+
+	//Renders texture at given point
+	void render(SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+	
+	void getSize(int& w, int& h);
+	int getWidth() { return width; }
+	int getHeight() { return height; }
+
+	float getX() { return x; }
+	float getY() { return y; }
+
+
+	void setRenderer(SDL_Renderer* renderer);
+
+	void setSize(int w, int h);
+	void setWith(int w);
+	void setHeight(int h);
+
+	float setX(float x);
+	float setY(float y);
+
+	//Deallocates texture
+	void free();
+
+	bool isEmpty() { return is_Empty; }
+
+protected:
+	int width, height;
+
+	SDL_Texture* texture;
+	SDL_Renderer* renderer;
+
+	float angle;
+	SDL_Point* center;
+	SDL_RendererFlip flip;
+
+	bool is_Empty;
+	float x, y;
+};
+
